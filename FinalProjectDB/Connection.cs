08a43854c76227826c87ff695102f5d0b4fb569d
hengkysanjaya <hengkysanjaya204@gmail.com>
2020-01-06 11:04:59 +0700

    Fix: add dependencies

---
+++ FinalProjectDB/Connection.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace FinalProjectDB
+{
+    public class Connection
+    {
+        MySqlConnection con;
+
+        public bool Connect()
+        {
+            try
+            {
+                string connection = "server=localhost;database=fp_db_nrhr;uid=root;pwd=";
+                con = new MySqlConnection(connection);
+                con.Open();
+                return true;
+            }
+            catch
+            {
+                Console.WriteLine("Connection Failed to open");
+                return false;
+            }
+        }
+
+    }
+}
